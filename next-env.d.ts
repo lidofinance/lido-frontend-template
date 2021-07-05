6e69cfaffbbbbb2b17de649c41e13390581b5a91
George Avsetsin <mail@sjors.ru>
2021-07-05 17:16:17 +0300

    feat: css prop

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="@lidofinance/theme" />
+/// <reference types="styled-components/cssprop" />
 
 declare module '*.svg' {
   import React = require('react');
