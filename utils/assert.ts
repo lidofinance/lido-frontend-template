11128998d7758a7d630526c3bc2b457453e98818
George Avsetsin <mail@sjors.ru>
2021-07-15 08:54:12 +0300

    refactor: assert

--- utils/assert.ts
+++ utils/assert.ts
@@ -2,16 +2,15 @@ import { assertionLogger } from './logger';
 
 export class AssertionError extends Error {}
 
-// eslint-disable-next-line func-style
-export function assert<T extends unknown>(
-  condition: T,
+export const assert: (
+  condition: unknown,
   event: string,
   errorMessage: string,
-): asserts condition {
+) => asserts condition = (condition, event, errorMessage) => {
   if (condition) return;
 
   const error = new AssertionError(errorMessage);
   assertionLogger.error({ event }, error);
 
   throw error;
-}
+};
