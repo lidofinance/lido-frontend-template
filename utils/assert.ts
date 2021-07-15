c20171ea83abc698b6c39b4af9e323bd8b31ba26
George Avsetsin <mail@sjors.ru>
2021-07-15 08:43:46 +0300

    fix: assert

--- utils/assert.ts
+++ utils/assert.ts
@@ -2,15 +2,16 @@ import { assertionLogger } from './logger';
 
 export class AssertionError extends Error {}
 
-export const assert = <T extends unknown>(
+// eslint-disable-next-line func-style
+export function assert<T extends unknown>(
   condition: T,
   event: string,
   errorMessage: string,
-): asserts condition => {
+): asserts condition {
   if (condition) return;
 
   const error = new AssertionError(errorMessage);
   assertionLogger.error({ event }, error);
 
   throw error;
-};
+}
