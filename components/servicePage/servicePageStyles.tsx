f6152e614a394755b5e168fdd7bdbf96295a6447
Anton Shalimov <a.shalimof@yandex.ru>
2021-10-14 15:47:56 +0300

    refactor(service page): use service page from ui lib

--- components/servicePage/servicePageStyles.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const ServicePageStyle = styled.div`
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  text-align: center;
-`;
--- components/servicePage/types.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from 'types';
-
-export type ServicePageComponent = Component<
-  'div',
-  {
-    title: React.ReactNode;
-  }
->;
