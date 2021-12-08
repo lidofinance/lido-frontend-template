2bd0589061079fcaa1cce28793dc98d3bf75088c
mymphe <azatsdev@gmail.com>
2021-12-08 13:49:48 +0600

    feat(monitoring): add build_info to metrics

--- pages/api/metrics.ts
+++ pages/api/metrics.ts
@@ -1,11 +1,14 @@
 import { METRICS_PREFIX } from 'config';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { collectDefaultMetrics, register } from 'prom-client';
+import { collectDefaultMetrics, Gauge, register } from 'prom-client';
+import buildInfoJson from 'build-info.json';
 
 type Metrics = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
 
 const metrics: Metrics = async (req, res) => {
   register.clear();
+
+  collectBuildInfo();
   collectDefaultMetrics({ prefix: METRICS_PREFIX });
 
   res.setHeader('Content-type', register.contentType);
@@ -14,3 +17,15 @@ const metrics: Metrics = async (req, res) => {
 };
 
 export default metrics;
+
+export const collectBuildInfo = (): void => {
+  const buildInfo = new Gauge({
+    name: METRICS_PREFIX + 'build_info',
+    help: 'Build information',
+    labelNames: ['version', 'commit', 'branch'],
+  });
+
+  const { version, commit, branch } = buildInfoJson;
+
+  buildInfo.labels(version, commit, branch).set(1);
+};
