cfd8729592c0d44dc707fd951560bce62b2a7920
mymphe <azatsdev@gmail.com>
2021-12-29 11:32:40 +0600

    feat(logging): use custom logger for production

--- utils/serverLogger.ts
+++ utils/serverLogger.ts
@@ -1,26 +1,54 @@
-import { createLogger, transports, format } from 'winston';
-import { traverse } from 'object-traversal';
-import { sanitizeMessage } from './sanitize';
+import maskString from '@darkobits/mask-string';
 
-const { json, timestamp, combine, errors } = format;
+// match any terra address except before 'contracts/'
+const userAddress = new RegExp('(?<!contracts/)terra[a-z0-9]+', 'gi');
+// match quiknode token, i.e. any alphanumeric characters after 'quiknode.pro/'
+const quikNodeToken = new RegExp('(?<=quiknode.pro/)[a-z0-9]+', 'gi');
 
-const sanitize = format((info) => {
-  traverse(info, (context) => {
-    const { parent, key, value } = context;
-    if (parent && key && typeof value === 'string') {
-      parent[key] = sanitizeMessage(value);
+const mask = (message: string): string =>
+  maskString([userAddress, quikNodeToken], message);
+
+enum LEVEL {
+  error = 'error',
+  warn = 'warn',
+  info = 'info',
+  debug = 'debug',
+}
+
+const stringify = (data: unknown) => {
+  let stringified = JSON.stringify(data);
+
+  if (data instanceof Error) {
+    // extract Error's non-enumerable props before stringifying
+    stringified = JSON.stringify({
+      message: data.message,
+      stack: data.stack,
+    });
+  }
+
+  return stringified;
+};
+
+const sanitize = (output: unknown[]) =>
+  JSON.parse(mask(JSON.stringify(output.map(stringify))));
+
+const log =
+  (level: LEVEL) =>
+  (...output: unknown[]): void => {
+    try {
+      console[level](sanitize(output));
+    } catch {
+      console.warn('Failed to sanitize output');
     }
-  });
-  return info;
-});
-
-const jsonLogger = createLogger({
-  defaultMeta: {
-    service: 'frontend_template',
-  },
-  format: combine(timestamp(), errors({ stack: true }), sanitize(), json()),
-  transports: [new transports.Console()],
-});
+  };
 
 export const serverLogger =
-  process.env.NODE_ENV === 'production' ? jsonLogger : console;
+  process.env.NODE_ENV === 'production'
+    ? {
+        error: log(LEVEL.error),
+        warn: log(LEVEL.warn),
+        info: log(LEVEL.info),
+        debug: log(LEVEL.debug),
+        log: log(LEVEL.debug),
+      }
+    : console;
