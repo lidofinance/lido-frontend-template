769210fd35791a4d38aafd8842873ce754d4a451
Miljan Milidrag <mm@shardlabs.io>
2022-01-23 16:25:09 +0100

    Tabs Component

---
+++ components/tabs/Tabs.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+import { TabWrapper, TabSlider, TabLabel } from './TabStyles';
+
+interface Props {
+  options: string[];
+  selected: string;
+  onSelectTab: React.MouseEventHandler<HTMLDivElement> &
+    ((option: string) => void);
+}
+
+const Switch: FC<Props> = ({ options, selected, onSelectTab }) => {
+  const selectedOption = options.indexOf(selected);
+  return (
+    <TabWrapper>
+      <TabSlider selectedOption={selectedOption} />
+      {options.map((option) => (
+        <TabLabel
+          isSelected={selected === option}
+          key={option}
+          onClick={() => onSelectTab(option)}
+        >
+          {option}
+        </TabLabel>
+      ))}
+    </TabWrapper>
+  );
+};
+
+export default Switch;
