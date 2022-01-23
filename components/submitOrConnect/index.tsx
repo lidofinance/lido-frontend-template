3da48dad2ede5f3ce4d4c07424788203416b6e8f
Miljan Milidrag <mm@shardlabs.io>
2022-01-23 16:24:23 +0100

    SubmitOrConnect Component

---
+++ components/submitOrConnect/index.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import { Button } from '@lidofinance/lido-ui';
+import WalletConnect from 'components/walletConnect/walletConnect';
+import { useSDK } from '@lido-sdk/react';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+  margin-top: ${({ theme }) => theme.spaceMap.md}px;
+  margin-bottom: ${({ theme }) => theme.spaceMap.xxl}px;
+`;
+
+interface Props {
+  isLoading: boolean;
+  label: string;
+  submit?: () => void;
+}
+
+const SubmitOrConnect: FC<Props> = ({ isLoading, label, submit }) => {
+  const { account } = useSDK();
+  return (
+    <ButtonWrapper>
+      {account ? (
+        <Button
+          fullwidth
+          type="submit"
+          disabled={isLoading}
+          loading={isLoading}
+          onClick={submit}
+        >
+          {label}
+        </Button>
+      ) : (
+        <WalletConnect fullwidth />
+      )}
+    </ButtonWrapper>
+  )
+};
+
+export default SubmitOrConnect;
