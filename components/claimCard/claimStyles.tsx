75a2af7e1a890f862d589b724ee3517719af9c98
Miljan Milidrag <mm@shardlabs.io>
2022-01-23 16:25:59 +0100

    ClaimCard Component

---
+++ components/claimCard/claimStyles.tsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const ClaimCardStyle = styled.div`
+  border: 1px solid #000a3d1f;
+  border-radius: 10px;
+  margin-bottom: 16px;
+`;
+
+export const ClaimStatStyle = styled.div`
+  display: flex;
+  justify-content: space-around;
+  background-color: #f5f7fa;
+  border-radius: 10px;
+  padding: 20px;
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
+  border-bottom: 1px solid #000a3d1f;
+`;
+
+export const ClaimStatBalanceStyle = styled.div`
+  margin-right: 18px;
+  font-size: ${({ theme }) => theme.fontSizesMap.xxs}px;
+  line-height: 1.6em;
+
+  :last-child {
+    margin-right: 0;
+  }
+`;
+
+export const ClaimCardTitleStyle = styled.div``;
+
+export const ClaimStatValueStyle = styled.div<{
+  $small: boolean;
+  $bold: boolean;
+}>`
+  margin-top: 2px;
+  font-size: ${({ theme, $small }) =>
+    $small ? theme.fontSizesMap.sm : theme.fontSizesMap.md}px;
+  line-height: 1.4em;
+  white-space: nowrap;
+  font-weight: ${({ $bold }) => ($bold ? 700 : 400)};
+  color: #273852;
+`;
+
+export const ClaimCardEditStyle = styled.div`
+  color: #00a3ff;
+  text-align: center;
+  padding: 12px 0;
+  cursor: pointer;
+`;
+
+export const ClaimCardExtraStyle = styled.div`
+  margin-top: 2px;
+  opacity: 0.5;
+`;
+
+export const ClaimCardContentStyle = styled.div<{ $hidden: boolean }>`
+  margin-top: 8px;
+  opacity: ${({ $hidden }) => ($hidden ? 0 : 1)};
+  pointer-events: ${({ $hidden }) => ($hidden ? 'none' : 'auto')};
+`;
---
+++ components/claimCard/types.ts
@@ -0,0 +1,21 @@
+import { Component } from 'types';
+import { BlockProps } from '@lidofinance/lido-ui';
+import { FC } from 'react';
+
+export type ClaimCardComponent = FC<BlockProps>;
+
+export type ClaimStatComponent = Component<'div'>;
+
+export type ClaimStatBalanceComponent = Component<
+  'div',
+  {
+    title: React.ReactNode;
+    value: React.ReactNode;
+    small?: boolean;
+    loading?: boolean;
+    bold?: boolean;
+    extra?: React.ReactNode;
+  }
+>;
+
+export type ClaimCardEditComponent = Component<'div'>;
