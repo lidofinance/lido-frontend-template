6cf1eec0b8908b12daf0535983fe33ffc90312cd
IvanBarbaric992 <ivan.barbaric992@gmail.com>
2022-02-17 18:50:08 +0100

    fix styled components warning

--- components/progressBar/progressBar.tsx
+++ components/progressBar/progressBar.tsx
@@ -5,20 +5,21 @@ type ProgressBarProps = {
   completed: number;
 };
 
+const Container = styled.div`
+  max-width: 252px;
+  height: 4px;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 8px;
+`;
+const Filler = styled.div`
+  height: 100%;
+  background-color: #53ba95;
+  opacity: 1;
+  border-radius: inherit;
+`;
+
 const ProgressBar: FC<ProgressBarProps> = ({ completed }) => {
-  const Container = styled.div`
-    max-width: 252px;
-    height: 4px;
-    width: 100%;
-    background-color: rgba(255, 255, 255, 0.1);
-    border-radius: 8px;
-  `;
-  const Filler = styled.div`
-    height: 100%;
-    background-color: #53ba95;
-    opacity: 1;
-    border-radius: inherit;
-  `;
   return (
     <Container>
       <Filler style={{ width: `${completed}%` }}></Filler>
