bd7a212992aba266b27a1fbe2d04e2f4cd617f2c
Anton Shalimov <a.shalimof@yandex.ru>
2022-11-08 19:29:30 +0300

    feat(config): dynamics envs

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,6 @@
+import getConfig from 'next/config';
+export const { serverRuntimeConfig } = getConfig();
+export { default as dynamics } from './dynamics';
 export * from './api';
 export * from './locale';
 export * from './rpc';
