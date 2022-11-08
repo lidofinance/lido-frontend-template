15417a1b522433e10cf132c20bc6787aca40037b
Anton Shalimov <a.shalimof@yandex.ru>
2022-11-09 00:25:33 +0300

    feat(dynamics envs): generate script

--- env-dynamics.mjs
+++ env-dynamics.mjs
@@ -1,2 +1,10 @@
 /** @type string */
 export const matomoUrl = process.env.NEXT_PUBLIC_MATOMO_URL;
+/** @type number */
+export const defaultChain = parseInt(process.env.DEFAULT_CHAIN, 10) || 1;
+/** @type number[] */
+export const supportedChains = process.env?.SUPPORTED_CHAINS?.split(',').map(
+  (chainId) => parseInt(chainId, 10),
+) ?? [1, 4, 5];
+/** @type boolean */
+export const enableQaHelpers = !!process.env.ENABLE_QA_HELPERS;
