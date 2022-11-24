a249fd8b42bc2be125ab2e8961a553e0dee5fc42
Nikita Kozlov <nikita.kozlov@p2p.org>
2022-11-24 17:47:39 +0300

    feat: bumped packages & fixed related issues

--- lib/faqList.ts
+++ lib/faqList.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import remark from 'remark';
+import { remark } from 'remark';
 import html from 'remark-html';
 import externalLinks from 'remark-external-links';
 
