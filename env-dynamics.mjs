6aa1d4c7080f4ce190f3721084113b87b5ba06bc
Anton Shalimov <a.shalimof@yandex.ru>
2022-12-07 17:02:27 +0300

    feat: remove ENABLE_QA_HELPERS

--- env-dynamics.mjs
+++ env-dynamics.mjs
@@ -6,5 +6,3 @@ export const defaultChain = parseInt(process.env.DEFAULT_CHAIN, 10) || 1;
 export const supportedChains = process.env?.SUPPORTED_CHAINS?.split(',').map(
   (chainId) => parseInt(chainId, 10),
 ) ?? [1, 4, 5];
-/** @type boolean */
-export const enableQaHelpers = !!process.env.ENABLE_QA_HELPERS;
