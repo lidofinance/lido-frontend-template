f0233ceb06a3874a5ab7605c943eaeb0dd70d4b8
Anton Shalimov <a.shalimof@yandex.ru>
2022-12-16 14:10:57 +0300

    chore: comments

--- config/matomoWalletsEvents.ts
+++ config/matomoWalletsEvents.ts
@@ -1,13 +1,13 @@
 import { MatomoEventType, trackEvent } from '@lidofinance/analytics-matomo';
 import { Metrics as WalletsMetrics } from 'reef-knot';
 
-// Should be change for Lido on X project!!!
+// Please, change the string to be relevant to your project.
 // For example:
 // - Ethereum_Staking_Widget
 // - Kusama_Stacking_Widget
 const MATOMO_EVENT_CATEGORY = 'Lido_Frontend_Template';
 
-// Should be change for Lido on X project!!!
+// Please, change the string to be relevant to your project.
 // For example:
 // - eth_widget_
 // - ksm_widget_
