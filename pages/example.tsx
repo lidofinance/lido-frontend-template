4dd8202225912f88c5ced533f0e32f595e00d3dd
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-10 15:44:07 +0300

    feat: add example page

---
+++ pages/example.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import Head from 'next/head';
+
+import Layout from 'common/layout';
+
+const Example: FC = () => {
+  return (
+    <Layout title="Example Page" subtitle="Just for routing test">
+      <Head>
+        <title>Lido | Frontend Template</title>
+      </Head>
+    </Layout>
+  );
+};
+
+export default Example;
