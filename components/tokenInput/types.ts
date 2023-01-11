e7bbe7c05275b73853f3a9239b7d2a7e0b6e9ed7
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-11 16:41:19 +0300

    refactor(TokenInput): remove

--- components/tokenInput/types.ts
+++
@@ -1,7 +0,0 @@
-import { InputProps } from '@lidofinance/lido-ui';
-import { Component } from 'types';
-
-export type TokenInputComponent = Component<
-  'input',
-  InputProps & { onMax: () => void }
->;
