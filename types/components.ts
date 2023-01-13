e0f2b1aaf73fd353530e2dae51607abe340ebe56
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-13 14:48:45 +0300

    docs: add todo

--- types/components.ts
+++ types/components.ts
@@ -2,6 +2,8 @@ import { AppProps } from 'next/app';
 import { EnvConfig } from 'common/providers';
 import { FC, ReactNode } from 'react';
 
+// todo: will be splitted
+
 export type ComponentProps<
   T extends keyof JSX.IntrinsicElements,
   P extends Record<string, unknown> = { children?: ReactNode },
