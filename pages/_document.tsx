99a9fc6b64921bb41671ca40aaf61136a92d8d45
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-17 13:50:46 +0300

    refactor(_document): class MyDocument

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,23 +6,21 @@ import Document, {
   DocumentContext,
   DocumentInitialProps,
 } from 'next/document';
-import { Fonts, LidoUIHead } from '@lidofinance/lido-ui';
 import { ServerStyleSheet } from 'styled-components';
+import { Fonts, LidoUIHead } from '@lidofinance/lido-ui';
+
 import { dynamics } from '../config';
 
-// for prod and dev use https and real domain
-let host = 'http://localhost';
+let host: string;
 
 export default class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext,
   ): Promise<DocumentInitialProps> {
+    host = `https://${ctx?.req?.headers?.host}`;
+
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-
-    if (ctx?.req?.headers?.host) {
-      host = `https://${ctx?.req?.headers?.host}`;
-    }
 
     try {
       ctx.renderPage = () =>
@@ -47,19 +45,14 @@ export default class MyDocument extends Document {
     }
   }
 
-  get metaTitle(): string {
-    return 'Lido Frontend Template | Lido';
-  }
+  private static metaTitle = 'Lido Frontend Template | Lido';
 
-  get metaDescription(): string {
-    return (
-      'Liquid staking with Lido. ' +
-      'Stake {TOKEN} with Lido to earn daily rewards while keeping full control of your staked tokens. ' +
-      'Start earning rewards in just a few clicks.'
-    );
-  }
+  private static metaDescription =
+    'Liquid staking with Lido. ' +
+    'Stake {TOKEN} with Lido to earn daily rewards while keeping full control of your staked tokens. ' +
+    'Start earning rewards in just a few clicks.';
 
-  get metaPreviewImgUrl(): string {
+  private static get metaPreviewImgUrl(): string {
     return `${host}/lido-preview.png`;
   }
 
@@ -69,9 +62,15 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" type="image/svg+xml" href="favicon-512x512.svg" />
+          <link
+            rel="icon"
+            type="image/svg+xml"
+            sizes="512x512"
+            href="/favicon-512x512.svg"
+          />
           <link
             rel="apple-touch-icon"
+            type="image/png"
             sizes="180x180"
             href="/apple-touch-icon.png"
           />
@@ -93,18 +92,28 @@ export default class MyDocument extends Document {
             sizes="16x16"
             href="/favicon-16x16.png"
           />
+          {/*  */}
           <meta name="description" content="Lido Template" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content={this.metaTitle} />
-          <meta property="og:description" content={this.metaDescription} />
-          <meta property="og:image" content={this.metaPreviewImgUrl} />
+          <meta property="og:title" content={MyDocument.metaTitle} />
+          <meta
+            property="og:description"
+            content={MyDocument.metaDescription}
+          />
+          <meta property="og:image" content={MyDocument.metaPreviewImgUrl} />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content={this.metaTitle} />
-          <meta name="twitter:description" content={this.metaDescription} />
-          <meta name="twitter:image:src" content={this.metaPreviewImgUrl} />
+          <meta name="twitter:title" content={MyDocument.metaTitle} />
+          <meta
+            name="twitter:description"
+            content={MyDocument.metaDescription}
+          />
+          <meta
+            name="twitter:image:src"
+            content={MyDocument.metaPreviewImgUrl}
+          />
           <meta name="twitter:site" content="@lidofinance" />
           <meta name="twitter:creator" content="@lidofinance" />
-          <meta name="description" content={this.metaDescription} />
+          <meta name="description" content={MyDocument.metaDescription} />
           <meta name="currentChain" content={String(dynamics.defaultChain)} />
           <Fonts />
           <LidoUIHead />
