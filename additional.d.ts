81e252a6fdf10a8be0139693b1aaf41c23385fe1
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-17 14:30:50 +0300

    docs(additional): todo

--- additional.d.ts
+++ additional.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="@lidofinance/theme" />
 /// <reference types="styled-components/cssprop" />
 
+// TODO: move common or remove?
 declare module '*.svg' {
   import React = require('react');
   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
@@ -8,6 +9,7 @@ declare module '*.svg' {
   export default src;
 }
 
+// TODO: refactor and move (for details by Vsevolod)
 type RuntimeConfig = {
   basePath?: string;
   infuraApiKey?: string;
