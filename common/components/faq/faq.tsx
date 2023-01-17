b341f9f5c9353187247b5a34f2275c0ff0c1bdc4
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-17 13:24:15 +0300

    refactor(FAQ): todo

--- common/components/faq/faq.tsx
+++ common/components/faq/faq.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Accordion } from '@lidofinance/lido-ui';
 
-import Section from 'common/components/section';
+// import Section from 'common/components/section';
 
 import { FaqProps } from './types';
 import { FaqItem } from './faqStyles';
@@ -10,7 +10,9 @@ const Faq: FC<FaqProps> = (props) => {
   const { faqList } = props;
 
   return (
-    <Section title="FAQ">
+    // TODO: {/* <Section > */} to top level
+    // <Section title="FAQ">
+    <>
       {faqList.map(({ id, title, content }, index) => (
         <Accordion
           key={id}
@@ -24,7 +26,8 @@ const Faq: FC<FaqProps> = (props) => {
           />
         </Accordion>
       ))}
-    </Section>
+    </>
+    //</Section>
   );
 };
 
