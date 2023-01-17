c769d80ec2e5cbcb10a7fb838a6bc8d6a624ad50
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-17 13:08:39 +0300

    refactor(global styles): css vars and style structuring

--- common/styles/global.ts
+++ common/styles/global.ts
@@ -5,29 +5,33 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
-  html,
-  body {
+
+  html, body {
     width: 100%;
   }
+
   body {
-    background: ${({ theme }) => theme.colors.background};
-    color: ${({ theme }) => theme.colors.text};
     position: relative;
     box-sizing: border-box;
+
     font-size: ${({ theme }) => theme.fontSizesMap.xs}px;
     line-height: 1.5em;
     font-weight: 500;
+    // https://developer.mozilla.org/en-US/docs/Web/CSS/text-size-adjust
     text-size-adjust: none;
+
+    background: var(--lido-color-background);
+    color: var(--lido-color-text);
   }
   a {
-    color: ${({ theme }) => theme.colors.primary};
+    color: var(--lido-color-primary);
 
     &:hover {
-      color: ${({ theme }) => theme.colors.primaryHover};
+      color: var(--lido-color-primaryHover);
     }
 
     &:visited {
-      color: ${({ theme }) => theme.colors.primaryVisited};
+      color: var(--lido-color-primaryVisited);
     }
   }
 `;
