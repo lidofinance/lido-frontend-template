b958787990a35a7088171973a2a557df8c5115d7
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-23 18:07:03 +0300

    refactor(api/health): using health from '@lidofinance/next-pages'

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -1,9 +1,5 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { health } from '@lidofinance/next-pages';
 
-// TODO: use https://github.com/lidofinance/warehouse/blob/main/packages/next/pages/src/health.ts
-export default function health(
-  req: NextApiRequest,
-  res: NextApiResponse,
-): void {
-  res.status(200).send({ status: 'ok' });
-}
+// More detail
+// https://github.com/lidofinance/warehouse/blob/main/packages/next/pages/src/health.ts
+export default health;
