8ee16fb02b6d25a20351d3dc423464918ce06eab
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-23 19:27:33 +0300

    docs: todo

--- pages/api/csp-report.ts
+++ pages/api/csp-report.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { serverLogger } from 'common/utils/serverLogger';
 
-// TODO: to sentry soon
 export default function cspReport(
   req: NextApiRequest,
   res: NextApiResponse,
 ): void {
+  // TODO: send to sentry
   serverLogger.warn({
     message: 'CSP Violation',
     report: JSON.parse(req.body),
