191e607884d06b20029b37fc22f7dbb976f85499
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-23 15:17:48 +0300

    fix: export Web3Provider

--- common/providers/index.ts
+++ common/providers/index.ts
@@ -1,4 +1,4 @@
 export { default as ModalProvider } from './modals';
 export * from './modals';
-export { default as Web3Provider } from './modals';
+export { default as Web3Provider } from './web3';
 export * from './web3';
