7ca114f4f47b63c00dd61a090c887888099a83a2
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-28 21:48:05 +0300

    refactor: unable to import svg files

--- additional.d.ts
+++ additional.d.ts
@@ -1,18 +1,10 @@
 /// <reference types="@lidofinance/theme" />
 /// <reference types="styled-components/cssprop" />
 
-// TODO: move common or remove?
+// Unable to import svg files in typescript like ```import svgFile from './svgFile.svg'```
 declare module '*.svg' {
   import React = require('react');
   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
   const src: string;
   export default src;
 }
-
-// TODO: refactor and move (for details by Vsevolod)
-type RuntimeConfig = {
-  basePath?: string;
-  infuraApiKey?: string;
-  alchemyApiKey?: string;
-  apiProviderUrls?: Record<number, string>;
-};
