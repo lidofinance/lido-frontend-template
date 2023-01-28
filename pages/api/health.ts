c1ed92538115e19316827bc1f9a60037ce69b2e0
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-28 21:58:56 +0300

    refactor(api): health

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -1,5 +1,3 @@
-import { health } from '@lidofinance/next-pages';
-
 // More detail
 // https://github.com/lidofinance/warehouse/blob/main/packages/next/pages/src/health.ts
-export default health;
+export { health as default } from '@lidofinance/next-pages';
