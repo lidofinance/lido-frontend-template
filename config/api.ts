44b65f4c6a6a8e2802d0944f73eb5d1ffe8cd193
Anton Shalimov <a.shalimof@yandex.ru>
2023-01-28 22:18:59 +0300

    refactor: removed not used

--- config/api.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_API_ERROR_MESSAGE =
-  'Something went wrong. Sorry, try again later :(';
--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,6 @@
 import getConfig from 'next/config';
 export const { serverRuntimeConfig } = getConfig();
 export { default as dynamics } from './dynamics';
-export * from './api';
-export * from './locale';
 export * from './rpc';
 export * from './example';
 export * from './headerNavigations';
