a5853d0adf6cb824df49be102f7ad5e1b3518c1a
Anton Shalimov <a.shalimof@yandex.ru>
2023-02-07 15:50:38 +0300

    refactor(navs): key

--- @common/layout/header/components/navigation/navigation.tsx
+++ @common/layout/header/components/navigation/navigation.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useId } from 'react';
+import { FC, memo } from 'react';
 
 import NavigationLink, { INavigationLink } from './components/navigationLink';
 import { NavigationStyle } from './styles';
@@ -7,16 +7,14 @@ import { INavigation } from './types';
 const Navigation: FC<INavigation> = memo((props: INavigation) => {
   const { pages } = props;
 
-  const id = useId();
-
   if (!pages) {
     return <></>;
   }
 
   return (
     <NavigationStyle>
-      {pages.map((pageProps: INavigationLink, index: number) => (
-        <NavigationLink key={`${id}-${index}`} {...pageProps} />
+      {pages.map((pageProps: INavigationLink) => (
+        <NavigationLink key={pageProps.href} {...pageProps} />
       ))}
     </NavigationStyle>
   );
