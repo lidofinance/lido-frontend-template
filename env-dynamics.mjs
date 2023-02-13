87fda9034f458f43210429a4053779359b540dd0
Anton Shalimov <a.shalimof@yandex.ru>
2023-02-13 18:43:12 +0300

    feat: toBoolean for process.env

--- env-dynamics.mjs
+++ env-dynamics.mjs
@@ -1,3 +1,16 @@
+/**
+ * Convert to bool:
+ * - true to true
+ * - 'true' to true
+ * - 1 to true
+ * - '1' to true
+ * - another values to false
+ * @returns {Boolean}
+ */
+const toBoolean = (dataStr) => {
+  return !!(dataStr?.toLowerCase?.() === 'true' || dataStr === true || Number.parseInt(dataStr, 10) === 1);
+};
+
 /** @type string */
 export const matomoHost = process.env.MATOMO_HOST;
 /** @type number */
@@ -6,3 +19,7 @@ export const defaultChain = parseInt(process.env.DEFAULT_CHAIN, 10) || 1;
 export const supportedChains = process.env?.SUPPORTED_CHAINS?.split(',').map(
   (chainId) => parseInt(chainId, 10),
 ) ?? [1, 4, 5];
+
+// Example!
+/** @type boolean */
+// export const enableQaHelpers = toBoolean(process.env.ENABLE_QA_HELPERS);
