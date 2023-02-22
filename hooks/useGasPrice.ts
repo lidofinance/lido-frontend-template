4e79a0f238faf364fe161ca1022531e54c714fb7
Anton Shalimov <a.shalimof@yandex.ru>
2023-02-22 13:43:38 +0300

    fix(hooks): useGasPrice

--- hooks/useGasPrice.ts
+++ hooks/useGasPrice.ts
@@ -4,14 +4,17 @@ import { BigNumber } from 'ethers';
 import { useSDK } from '@lido-sdk/react';
 import { getStaticRpcBatchProvider } from '@lido-sdk/providers';
 
-import { ONE_GWEI } from 'consts';
+import { ONE_GWEI, getBackendRPCPath } from 'consts';
 
 export const useGasPrice = (): BigNumber | undefined => {
   const [gasPrice, setGasPrice] = useState<BigNumber>();
   const { chainId } = useSDK();
 
   const getGasPrice = useCallback(async () => {
-    const staticProvider = getStaticRpcBatchProvider(chainId, '/api/rpc');
+    const staticProvider = getStaticRpcBatchProvider(
+      chainId,
+      getBackendRPCPath(chainId),
+    );
 
     try {
       const newGasPrice = await staticProvider.getGasPrice();
