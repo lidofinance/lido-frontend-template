34febc3da16e15deb4637c03fd22af83d142187b
Anton Shalimov <a.shalimof@yandex.ru>
2023-02-22 13:40:46 +0300

    refactor(useGasPrice): using staticRpcBatchProvider

--- hooks/useGasPrice.ts
+++ hooks/useGasPrice.ts
@@ -2,28 +2,28 @@ import { useCallback, useEffect, useState } from 'react';
 import { BigNumber } from 'ethers';
 
 import { useSDK } from '@lido-sdk/react';
+import { getStaticRpcBatchProvider } from '@lido-sdk/providers';
 
 import { ONE_GWEI } from 'consts';
 
 export const useGasPrice = (): BigNumber | undefined => {
   const [gasPrice, setGasPrice] = useState<BigNumber>();
-  const { providerRpc } = useSDK();
+  const { chainId } = useSDK();
 
   const getGasPrice = useCallback(async () => {
-    if (providerRpc) {
-      try {
-        // TODO: use staticRpcBatchProvider
-        const newGasPrice = await providerRpc.getGasPrice();
-        setGasPrice(newGasPrice);
-      } catch (e) {
-        console.error(e);
-        setGasPrice(ONE_GWEI);
-      }
+    const staticProvider = getStaticRpcBatchProvider(chainId, '/api/rpc');
+
+    try {
+      const newGasPrice = await staticProvider.getGasPrice();
+      setGasPrice(newGasPrice);
+    } catch (e) {
+      console.error(e);
+      setGasPrice(ONE_GWEI);
     }
-  }, [providerRpc]);
+  }, [chainId]);
 
   useEffect(() => {
-    getGasPrice();
+    void getGasPrice();
   }, [getGasPrice]);
 
   return gasPrice;
