d768b515516e16e1a63a20cedf3fdcbf629b5cb8
Anton Shalimov <a.shalimof@yandex.ru>
2023-03-27 23:51:11 +0300

    refactor: consts

--- consts/cache.ts
+++ consts/cache.ts
@@ -1,14 +1,2 @@
-import ms from 'ms';
-
-export const CACHE_STETH_APR_KEY = 'cache-steth-apr';
-export const CACHE_STETH_APR_TTL = ms('1h');
-
-export const CACHE_LIDO_SHORT_STATS_KEY = 'cache-short-lido-stats';
-export const CACHE_LIDO_SHORT_STATS_TTL = ms('1h');
-
-export const CACHE_LIDO_HOLDERS_VIA_SUBGRAPHS_KEY =
-  'cache-lido-holders-via-subgraphs';
-export const CACHE_LIDO_HOLDERS_VIA_SUBGRAPHS_TTL = ms('7d');
-
 export const CACHE_DEFAULT_HEADERS =
   'public, max-age=180, stale-if-error=1200, stale-while-revalidate=60';
--- consts/chains.ts
+++ consts/chains.ts
@@ -4,5 +4,3 @@ export type MAINNET_AND_GOERLI_CHAINS = Extract<
   CHAINS,
   CHAINS.Mainnet | CHAINS.Goerli
 >;
-
-export type SUBGRAPH_CHAINS = MAINNET_AND_GOERLI_CHAINS;
