c72cb9070132dba51f2e230ce34d219a06b5dc6a
Anton Shalimov <a.shalimof@yandex.ru>
2023-03-28 16:10:49 +0300

    refactor: rpc order

--- utilsApi/rpcUrls.ts
+++ utilsApi/rpcUrls.ts
@@ -5,11 +5,11 @@ const { infuraApiKey, alchemyApiKey } = serverRuntimeConfig;
 
 export const rpcUrls: Record<string | number, [string, ...string[]]> = {
   [CHAINS.Mainnet]: [
-    `https://mainnet.infura.io/v3/${infuraApiKey}`,
     `https://eth-mainnet.alchemyapi.io/v2/${alchemyApiKey}`,
+    `https://mainnet.infura.io/v3/${infuraApiKey}`,
   ],
   [CHAINS.Goerli]: [
-    `https://goerli.infura.io/v3/${infuraApiKey}`,
     `https://eth-goerli.alchemyapi.io/v2/${alchemyApiKey}`,
+    `https://goerli.infura.io/v3/${infuraApiKey}`,
   ],
 };
