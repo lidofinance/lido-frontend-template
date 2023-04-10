db5e4d2186322f71ee266b80b6886b188033766a
Anton Shalimov <a.shalimof@yandex.ru>
2023-04-10 16:37:28 +0300

    chore(deps): update .yalc

--- .yalc/@lidofinance/next-ui-pages/dist/index.cjs
+++ .yalc/@lidofinance/next-ui-pages/dist/index.cjs
@@ -36,8 +36,11 @@ const $a3477cef5750232b$export$ece78e4633e29053 = ({ title: title , content: con
         title: title,
         children: [
             /*#__PURE__*/ (0, $RCG3D$reactjsxruntime.jsx)((0, ($parcel$interopDefault($RCG3D$nexthead))), {
-                children: /*#__PURE__*/ (0, $RCG3D$reactjsxruntime.jsx)("title", {
-                    children: "Lido | Page Not Found"
+                children: /*#__PURE__*/ (0, $RCG3D$reactjsxruntime.jsxs)("title", {
+                    children: [
+                        "Lido | ",
+                        title
+                    ]
                 })
             }),
             content
@@ -45,7 +48,39 @@ const $a3477cef5750232b$export$ece78e4633e29053 = ({ title: title , content: con
     });
 
 
+var $897c4d10c77c9d43$exports = {};
+
+$parcel$export($897c4d10c77c9d43$exports, "Page404", function () { return $897c4d10c77c9d43$export$3cb69218ff952ef8; });
+
+
+
+const $897c4d10c77c9d43$var$DefaultContent = ()=>/*#__PURE__*/ (0, $RCG3D$reactjsxruntime.jsx)((0, $RCG3D$reactjsxruntime.Fragment), {
+        children: "Page Not Found \xaf\\_(ツ)_/\xaf'"
+    });
+const $897c4d10c77c9d43$export$3cb69218ff952ef8 = ({ title: title , content: content  })=>/*#__PURE__*/ (0, $RCG3D$reactjsxruntime.jsx)((0, $a3477cef5750232b$export$ece78e4633e29053), {
+        title: title !== null && title !== void 0 ? title : "404",
+        content: content !== null && content !== void 0 ? content : /*#__PURE__*/ (0, $RCG3D$reactjsxruntime.jsx)($897c4d10c77c9d43$var$DefaultContent, {})
+    });
+
+
+var $ba7842c3da55dc2b$exports = {};
+
+$parcel$export($ba7842c3da55dc2b$exports, "Page500", function () { return $ba7842c3da55dc2b$export$fd3f669c2104219d; });
+
+
+
+const $ba7842c3da55dc2b$var$DefaultContent = ()=>/*#__PURE__*/ (0, $RCG3D$reactjsxruntime.jsx)((0, $RCG3D$reactjsxruntime.Fragment), {
+        children: "Oops! Something went wrong.'"
+    });
+const $ba7842c3da55dc2b$export$fd3f669c2104219d = ({ title: title , content: content  })=>/*#__PURE__*/ (0, $RCG3D$reactjsxruntime.jsx)((0, $a3477cef5750232b$export$ece78e4633e29053), {
+        title: title !== null && title !== void 0 ? title : "500",
+        content: content !== null && content !== void 0 ? content : /*#__PURE__*/ (0, $RCG3D$reactjsxruntime.jsx)($ba7842c3da55dc2b$var$DefaultContent, {})
+    });
+
+
 $parcel$exportWildcard(module.exports, $a3477cef5750232b$exports);
+$parcel$exportWildcard(module.exports, $897c4d10c77c9d43$exports);
+$parcel$exportWildcard(module.exports, $ba7842c3da55dc2b$exports);
 
 
 //# sourceMappingURL=index.cjs.map
--- .yalc/@lidofinance/next-ui-pages/dist/index.mjs
+++ .yalc/@lidofinance/next-ui-pages/dist/index.mjs
@@ -1,4 +1,4 @@
-import {jsxs as $5MjI2$jsxs, jsx as $5MjI2$jsx} from "react/jsx-runtime";
+import {jsxs as $5MjI2$jsxs, jsx as $5MjI2$jsx, Fragment as $5MjI2$Fragment} from "react/jsx-runtime";
 import "react";
 import $5MjI2$nexthead from "next/head";
 import {ServicePage as $5MjI2$ServicePage} from "@lidofinance/lido-ui";
@@ -17,8 +17,11 @@ const $1415d00c5931e9d4$export$ece78e4633e29053 = ({ title: title , content: con
         title: title,
         children: [
             /*#__PURE__*/ (0, $5MjI2$jsx)((0, $5MjI2$nexthead), {
-                children: /*#__PURE__*/ (0, $5MjI2$jsx)("title", {
-                    children: "Lido | Page Not Found"
+                children: /*#__PURE__*/ (0, $5MjI2$jsxs)("title", {
+                    children: [
+                        "Lido | ",
+                        title
+                    ]
                 })
             }),
             content
@@ -26,7 +29,37 @@ const $1415d00c5931e9d4$export$ece78e4633e29053 = ({ title: title , content: con
     });
 
 
+var $54c9481511c95e6d$exports = {};
+
+$parcel$export($54c9481511c95e6d$exports, "Page404", function () { return $54c9481511c95e6d$export$3cb69218ff952ef8; });
 
 
-export {$1415d00c5931e9d4$export$ece78e4633e29053 as PageError};
+
+const $54c9481511c95e6d$var$DefaultContent = ()=>/*#__PURE__*/ (0, $5MjI2$jsx)((0, $5MjI2$Fragment), {
+        children: "Page Not Found \xaf\\_(ツ)_/\xaf'"
+    });
+const $54c9481511c95e6d$export$3cb69218ff952ef8 = ({ title: title , content: content  })=>/*#__PURE__*/ (0, $5MjI2$jsx)((0, $1415d00c5931e9d4$export$ece78e4633e29053), {
+        title: title !== null && title !== void 0 ? title : "404",
+        content: content !== null && content !== void 0 ? content : /*#__PURE__*/ (0, $5MjI2$jsx)($54c9481511c95e6d$var$DefaultContent, {})
+    });
+
+
+var $9408d61a34a377d4$exports = {};
+
+$parcel$export($9408d61a34a377d4$exports, "Page500", function () { return $9408d61a34a377d4$export$fd3f669c2104219d; });
+
+
+
+const $9408d61a34a377d4$var$DefaultContent = ()=>/*#__PURE__*/ (0, $5MjI2$jsx)((0, $5MjI2$Fragment), {
+        children: "Oops! Something went wrong.'"
+    });
+const $9408d61a34a377d4$export$fd3f669c2104219d = ({ title: title , content: content  })=>/*#__PURE__*/ (0, $5MjI2$jsx)((0, $1415d00c5931e9d4$export$ece78e4633e29053), {
+        title: title !== null && title !== void 0 ? title : "500",
+        content: content !== null && content !== void 0 ? content : /*#__PURE__*/ (0, $5MjI2$jsx)($9408d61a34a377d4$var$DefaultContent, {})
+    });
+
+
+
+
+export {$1415d00c5931e9d4$export$ece78e4633e29053 as PageError, $54c9481511c95e6d$export$3cb69218ff952ef8 as Page404, $9408d61a34a377d4$export$fd3f669c2104219d as Page500};
 //# sourceMappingURL=index.mjs.map
