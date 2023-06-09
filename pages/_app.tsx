1744cc3ff9d1e1aa52f1f04621542cabfff589d4
Anton Shalimov <a.shalimof@yandex.ru>
2023-06-09 14:19:39 +0300

    feat: wagmi (draft)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import NextApp, { AppProps } from 'next/app';
+import getConfig from 'next/config';
 import { ProviderWeb3 } from '@reef-knot/web3-react';
 import { ProviderWalletModal } from '@lidofinance/eth-ui-wallet-modal';
 import {
@@ -12,7 +13,10 @@ import { WidgetApp, NavigationLinkProps } from '@lidofinance/next-widget-app';
 
 import { backendRPC, dynamics, walletsMetrics } from 'config';
 import { HeaderActions } from 'components/headerActions';
+import { AppWagmiConfig } from 'components/wagmi';
 import { withCsp } from 'utils';
+
+const { publicRuntimeConfig } = getConfig();
 
 // Migrations old allow cookies to new cross domain cookies
 // It is here 'cause `keyOldCookies` may be different for widgets
@@ -42,24 +46,27 @@ const headerNavigation: NavigationLinkProps[] = [
 
 // App wrapper
 const WidgetAppWrapper: FC<AppProps> = ({ ...props }) => (
-  // @ts-expect-error ProviderWeb3 accepts children, but some problem with type
-  <ProviderWeb3
-    defaultChainId={dynamics.defaultChain}
-    supportedChainIds={dynamics.supportedChains}
-    rpc={backendRPC}
-  >
-    <ProviderWalletModal
-      walletsMetrics={walletsMetrics}
-      hiddenWallets={['Opera Wallet']}
+  <AppWagmiConfig>
+    {/* @ts-expect-error ProviderWeb3 accepts children, but some problem with type */}
+    <ProviderWeb3
+      defaultChainId={dynamics.defaultChain}
+      supportedChainIds={dynamics.supportedChains}
+      rpc={backendRPC}
+      walletconnectProjectId={publicRuntimeConfig.walletconnectProjectId}
     >
-      <WidgetApp
-        navigation={headerNavigation}
-        headerActions={<HeaderActions />}
+      <ProviderWalletModal
+        walletsMetrics={walletsMetrics}
+        hiddenWallets={['Opera Wallet']}
       >
-        <NextApp {...props} />
-      </WidgetApp>
-    </ProviderWalletModal>
-  </ProviderWeb3>
+        <WidgetApp
+          navigation={headerNavigation}
+          headerActions={<HeaderActions />}
+        >
+          <NextApp {...props} />
+        </WidgetApp>
+      </ProviderWalletModal>
+    </ProviderWeb3>
+  </AppWagmiConfig>
 );
 
 export default process.env.NODE_ENV === 'development'
