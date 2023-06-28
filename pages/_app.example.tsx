b96a5b8f2a18a60c3bc5ec320ae6e2f685e38fd2
Anton Shalimov <a.shalimof@yandex.ru>
2023-06-28 12:44:27 +0300

    feat: not EVM widget app example

---
+++ pages/_app.example.tsx
@@ -0,0 +1,56 @@
+import { FC } from 'react';
+import NextApp, { AppProps } from 'next/app';
+import { ProviderWeb3 } from 'reef-knot/web3-react';
+
+import { ProviderWalletModal } from '@lidofinance/eth-ui-wallet-modal';
+import { Stake } from '@lidofinance/lido-ui';
+import { WidgetApp } from '@lidofinance/next-widget-app';
+import { NavigationLinkProps } from '@lidofinance/next-widget-layout';
+
+import { backendRPC, dynamics, walletsMetrics } from 'config';
+import { HeaderActions } from 'components/headerActions';
+import { GlobalStyles } from 'components/globalStyle';
+import { AppWagmiConfig } from 'components/wagmi';
+import NoSSRWrapper from 'components/no-ssr-wrapper';
+
+// Header pages
+const headerNavigation: NavigationLinkProps[] = [
+  {
+    title: 'Stake',
+    href: '/',
+    icon: <Stake />,
+  },
+];
+
+// App use not EVM wrapper.
+// In general, this is also an example for ETH, however, this structure can be used for any blockchain.
+const WidgetAppWrapper: FC<AppProps> = ({ ...props }) => (
+  <AppWagmiConfig>
+    {/* @ts-expect-error ProviderWeb3 accepts children, but some problem with type */}
+    <ProviderWeb3
+      defaultChainId={dynamics.defaultChain}
+      supportedChainIds={dynamics.supportedChains}
+      rpc={backendRPC}
+      walletconnectProjectId={dynamics.walletconnectProjectId}
+    >
+      {/* Temporary fix hydration error */}
+      {/* TODO */}
+      <NoSSRWrapper>
+        <ProviderWalletModal
+          walletsMetrics={walletsMetrics}
+          hiddenWallets={['Opera Wallet']}
+        >
+          <WidgetApp
+            navigation={headerNavigation}
+            headerActions={<HeaderActions />}
+          >
+            <GlobalStyles />
+            <NextApp {...props} />
+          </WidgetApp>
+        </ProviderWalletModal>
+      </NoSSRWrapper>
+    </ProviderWeb3>
+  </AppWagmiConfig>
+);
+
+export default WidgetAppWrapper;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import { FC } from 'react';
 import NextApp, { AppProps } from 'next/app';
-import getConfig from 'next/config';
-// import { ProviderWeb3 } from 'reef-knot/web3-react'
 
-// import { ProviderWalletModal } from '@lidofinance/eth-ui-wallet-modal';
 import {
   Stake,
   Ldo as LdoIcon,
   Wrap,
   migrationAllowCookieToCrossDomainCookieClientSide,
 } from '@lidofinance/lido-ui';
-// import { WidgetApp } from '@lidofinance/next-widget-app';
 import { WidgetAppEVM } from '@lidofinance/next-widget-app';
 import { NavigationLinkProps } from '@lidofinance/next-widget-layout';
 
@@ -21,13 +17,9 @@ import {
   walletsMetrics,
 } from 'config';
 import { HeaderActions } from 'components/headerActions';
-// import { AppWagmiConfig } from 'components/wagmi';
 import { GlobalStyles } from 'components/globalStyle';
 import NoSSRWrapper from 'components/no-ssr-wrapper';
 import { withCsp } from 'utils';
-
-// TODO: dynamics
-const { publicRuntimeConfig } = getConfig();
 
 // Migrations old allow cookies to new cross domain cookies
 // It is here 'cause `keyOldCookies` may be different for widgets
@@ -55,9 +47,10 @@ const headerNavigation: NavigationLinkProps[] = [
   },
 ];
 
-// App wrapper
+// App use EVM wrapper
 const WidgetAppWrapper: FC<AppProps> = ({ ...props }) => (
   // Temporary fix hydration error
+  // TODO
   <NoSSRWrapper>
     <WidgetAppEVM
       navigation={headerNavigation}
@@ -70,14 +63,14 @@ const WidgetAppWrapper: FC<AppProps> = ({ ...props }) => (
         defaultChainId: dynamics.defaultChain,
         supportedChainIds: dynamics.supportedChains,
         rpc: backendRPC,
-        walletconnectProjectId: publicRuntimeConfig.walletconnectProjectId,
+        walletconnectProjectId: dynamics.walletconnectProjectId,
       }}
       wagmi={{
         defaultChain: dynamics.defaultChain,
         supportedChains: dynamics.supportedChains,
         backendRPC: backendRPC,
         getBackendRPCPath: getBackendRPCPath,
-        walletconnectProjectId: publicRuntimeConfig.walletconnectProjectId,
+        walletconnectProjectId: dynamics.walletconnectProjectId,
       }}
     >
       <GlobalStyles />
@@ -85,34 +78,6 @@ const WidgetAppWrapper: FC<AppProps> = ({ ...props }) => (
     </WidgetAppEVM>
   </NoSSRWrapper>
 );
-
-// App wrapper
-// const WidgetAppWrapper: FC<AppProps> = ({ ...props }) => (
-//   <AppWagmiConfig>
-//     {/* @ts-expect-error ProviderWeb3 accepts children, but some problem with type */}
-//     <ProviderWeb3
-//       defaultChainId={dynamics.defaultChain}
-//       supportedChainIds={dynamics.supportedChains}
-//       rpc={backendRPC}
-//       walletconnectProjectId={publicRuntimeConfig.walletconnectProjectId}
-//     >
-//       {/* Temporary fix hydration error */}
-//       <NoSSRWrapper>
-//         <ProviderWalletModal
-//           walletsMetrics={walletsMetrics}
-//           hiddenWallets={['Opera Wallet']}
-//         >
-//           <WidgetApp
-//             navigation={headerNavigation}
-//             headerActions={<HeaderActions />}
-//           >
-//             <NextApp {...props} />
-//           </WidgetApp>
-//         </ProviderWalletModal>
-//       </NoSSRWrapper>
-//     </ProviderWeb3>
-//   </AppWagmiConfig>
-// );
 
 export default process.env.NODE_ENV === 'development'
   ? WidgetAppWrapper
